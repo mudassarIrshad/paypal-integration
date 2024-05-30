f27a09a7f5db920fd56d79a95299258fba8f9c7a
Mudassar125 <mudassarirshad124@gmail.com>
2024-05-30 16:34:29 +0500

    WIP: Fixed payment method in paypal controller

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Srmklive\PayPal\Services\PayPal as PayPalClient;
+use Srmklive\PayPal\Services\PayPal;
+//use Srmklive\PayPal\Services\PayPal;
 
 class PayPalController extends Controller
 {
     protected $paypal;
 
-    public function __construct(PayPalClient $paypal)
+    public function __construct(PayPal $paypal)
     {
         $this->paypal = $paypal;
     }
 
-    public function pay()
+    public function payment()
     {
         $data = [
             'items' => [
@@ -25,14 +26,24 @@ class PayPalController extends Controller
                 ]
             ],
             'invoice_id' => uniqid(),
-            'invoice_description' => "Order #{$data['invoice_id']} Invoice",
-            'return_url' => route('paypal.status'),
-            'cancel_url' => route('paypal.status'),
+            // Define $data['invoice_description'] here
+            'return_url' => route('payment.success'),
+            'cancel_url' => route('payment.success'),
             'total' => 10,
         ];
 
-        $response = $this->paypal->setExpressCheckout($data);
-        return redirect($response['paypal_link']);
+// Now you can reference $data['invoice_id'] within $data['invoice_description']
+        $data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
+        $provider = PayPal::setProvider();
+        $provider->setApiCredentials(config('paypal'));
+        $token = $provider->getAccessToken();
+        $provider->setAccessToken($token);
+//        PayPal::setProvider();
+//        $paypalProvider = PayPal::getProvider();
+//        $paypalProvider->setApiCredentials(config('paypal'));
+//        $paypalProvider->setAccessToken($paypalProvider->getAccessToken());
+//        $response = $this->paypal->setExpressCheckout($data);
+//        return redirect($response['paypal_link']);
     }
 
     public function status(Request $request)
