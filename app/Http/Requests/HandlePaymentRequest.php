436e1c3425c3b67030c6f24dcd7b7cb83a22cebd
Mudassar125 <mudassarirshad124@gmail.com>
2024-06-02 12:44:13 +0500

    Added HandlePaymentRequest

---
+++ app/Http/Requests/HandlePaymentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HandlePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'products' => ['required', 'array'],
+            'invoice_id' => ['required', 'string'],
+            'invoice_description' => ['required', 'string'],
+            'total' => ['required', 'integer']
+        ];
+    }
+}
